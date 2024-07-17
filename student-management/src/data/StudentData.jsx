37269670b4ee5c1975085b81a804e13cf13d8082
lawasardon <lawasmarkardon@gmail.com>
2024-07-17 14:43:18 +0800

    Student Management July/17/2024

---
+++ student-management/src/data/StudentData.jsx
@@ -0,0 +1,16 @@
+// src/data/studentsData.js
+
+const students = [
+    { name: 'Mark Ardon Lawas', department: 'Science', finalGrade: 75, status: 'Pass' },
+    { name: 'Ynna Lyn Naveo', department: 'Arts', finalGrade: 95, status: 'Pass' },
+    { name: 'Marc Aldwin Kasayan', department: 'Commerce', finalGrade: 70, status: 'Pass' },
+    { name: 'Rodel Celis', department: 'Science', status: 'Fail' },
+    { name: 'John Nicole Lawas', department: 'Arts', finalGrade: 72, status: 'Pass' },
+    { name: 'Rey Lawas', department: 'Commerce', finalGrade: 94, status: 'Pass' },
+    { name: 'Edwin Ortega', department: 'Science', finalGrade: 80, status: 'Pass' },
+    { name: 'Peter John Beroy', department: 'Commerce', finalGrade: 84, status: 'Pass' },
+    { name: 'Mark Eliezon Aniñon', department: 'Science', finalGrade: 68, status: 'Pass' },
+    { name: 'Jei Navarro', department: 'Arts', status: 'Fail' },
+];
+
+export default students;
